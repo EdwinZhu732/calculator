24c05899121baf324ee390f9b9b4d374d05a7570
EdwinZhu732 <edwinzhu595@gmail.com>
2023-05-11 13:44:13 -0400

    Coded basic functions and added buttons

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # calculator
+This is a simple browser calculator made with Javascript, HTML, and CSS. Done as part of the Odin Project.
---
+++ javascript.js
@@ -0,0 +1,36 @@
+let num1;
+let num2;
+let operator;
+let displayValue;
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+    //Need to add divide by zero case
+}
+
+function operate (operator, num1, num2){
+    if (operator == "+"){
+        return add(num1, num2);
+    }
+    else if (operator == "-"){
+        return subtract(num1, num2);
+    }
+    else if (operator == "*"){
+        return multiply(num1, num2);
+    }
+    else if (operator == "/"){
+        return divide(num1, num2);
+    }
+}
